76709cce82f8de7d0b92b93a2e3026d879dba89b
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-04-17 17:43:20 -0300

    feat: introduz as anotações do lombok

--- src/main/java/dev/lucasnascimento/SpringProject2/Missoes/MissoesModel.java
+++ src/main/java/dev/lucasnascimento/SpringProject2/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.lucasnascimento.SpringProject2.Missoes;
 
 import dev.lucasnascimento.SpringProject2.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
